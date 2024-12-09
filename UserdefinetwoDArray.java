9f46378301c2c889efad47ee9255ab2296789628
vishnu303 <Shramavishal303@gmail.com>
2024-12-09 13:48:14 +0530

    UserdefineArray

---
+++ UserdefinetwoDArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class UserdefinetwoDArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        
+        System.out.print("Enter the number of columns: ");
+        int cols = sc.nextInt();
+        
+        int[][] a = new int[rows][cols];
+        
+        // Input elements into the 2D array
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        
+        // Print the 2D array
+        System.out.println("The 2D array is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        // Close the Scanner to prevent resource leak
+    }
+}
