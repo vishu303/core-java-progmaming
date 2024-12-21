ab948df711d7f465e124d6dc9d5280a13252a783
vishnu303 <Shramavishal303@gmail.com>
2024-12-21 19:44:37 +0530

    Coolection to HashSet

---
+++ Collectionjava/Hashset/CollectiontoHashSet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CollectiontoHashSet{
+    public static void main(String[] args) {
+        Collection c=new HashSet<>();
+        c.add(23);
+        c.add(56);
+        System.out.println(c);
+    }
+    
+}
