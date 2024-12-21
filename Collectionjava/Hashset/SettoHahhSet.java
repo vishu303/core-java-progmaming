d5f0b1390b31aa80a955ba19ea47422ab4737d64
vishnu303 <Shramavishal303@gmail.com>
2024-12-21 19:50:21 +0530

    Set to HashSet

---
+++ Collectionjava/Hashset/SettoHahhSet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SettoHahhSet {
+    public static void main(String[] args) {
+        Set s=new HashSet<>();
+        s.add(34);
+        s.add("hii");
+        System.out.println(s);
+    }
+    
+}
