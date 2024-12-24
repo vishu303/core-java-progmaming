a1b4ae7849d3617c07b08e4e5debe953e767c2c5
vishnu303 <Shramavishal303@gmail.com>
2024-12-24 21:47:44 +0530

    WrapperClass

---
+++ Collectionjava/Treeset/WrapperClass.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class WrapperClass{
+    public static void main(String[] args) {
+        TreeSet ts=new TreeSet<>();
+        ts.add(12);
+        ts.add(34);
+        ts.add(23);
+        ts.add(45);
+        System.out.print(ts);
+    }
+}
