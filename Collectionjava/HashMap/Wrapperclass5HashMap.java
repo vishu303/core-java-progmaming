73cbfc3c6816c6de27479a1a609268b9390ad74f
vishnu303 <Shramavishal303@gmail.com>
2024-12-26 13:56:28 +0530

    Wrapperclass5HashMap

---
+++ Collectionjava/HashMap/Wrapperclass5HashMap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class  Wrapperclass5HashMap {
+    public static void main(String[] args) {
+          HashMap<String,Character>hs=new HashMap<>();
+          hs.put("one",'k');
+          hs.put("two",'h');
+          hs.put("three",'l');
+          System.out.println(hs);
+    }
+}
