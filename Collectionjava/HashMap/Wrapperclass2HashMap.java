acf0bb3ad41985f01772fc463fa260ede05f590e
vishnu303 <Shramavishal303@gmail.com>
2024-12-26 13:28:20 +0530

    Wrapperclass2HashMap

---
+++ Collectionjava/HashMap/Wrapperclass2HashMap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Wrapperclass2HashMap {
+    public static void main(String[] args) {
+         HashMap<String,integer>hs1=new HashMap<>();
+         hs1.put("vishal",23);
+         hs1.put("two",67);
+         System.out.println(hs1);
+
+    }
+}
