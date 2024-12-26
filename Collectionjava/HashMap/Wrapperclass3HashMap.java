79c177afbb0e949bdd24cece0b5c1a7ec37e3c5d
vishnu303 <Shramavishal303@gmail.com>
2024-12-26 13:48:07 +0530

    Wrapperclass3HashMap

---
+++ Collectionjava/HashMap/Wrapperclass3HashMap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Wrapperclass3HashMap {
+    public static void main(String[] args) {
+        HashMap<Float,String>hs=new HashMap<>();
+         hs.put(23.5f,"vishal");
+         hs.put(45.3f,"rohit");
+         hs.put(78.1f,"nihal");
+         System.out.println(hs);
+        }
+    
+}
