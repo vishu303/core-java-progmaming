85ea8f28269b4541f79af35afee14e64178209b8
vishnu303 <Shramavishal303@gmail.com>
2024-12-27 20:16:19 +0530

    HomogenousLinkedHashMap

---
+++ Collectionjava/HashMap/LinkedHashMap/HomogenousLinkedHashMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HomogenousLinkedHashMap {
+   public static void main(String[] args) {
+    LinkedHashMap lhm=new LinkedHashMap<>();
+    lhm.put(1,"hii");
+    lhm.put(2,"yellow");
+    lhm.put(3,"bye");
+    System.out.println(lhm);
+   } 
+}
