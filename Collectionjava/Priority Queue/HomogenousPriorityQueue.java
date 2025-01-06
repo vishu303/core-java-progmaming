79d24f180c7857628f5b291fd69919a3b35e6dc9
vishnu303 <Shramavishal303@gmail.com>
2025-01-06 17:24:48 +0530

    HomogenousPriorityQueue

---
+++ Collectionjava/Priority Queue/HomogenousPriorityQueue.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class HomogenousPriorityQueue {
+ public static void main(String[] args) {
+    Queue<Integer>q = new PriorityQueue<>();
+    q.add(34);
+    q.add(78);
+    q.add(90);
+    System.out.println(q);
+ }   
+}
