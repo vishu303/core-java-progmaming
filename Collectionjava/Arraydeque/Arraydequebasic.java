50b19cc80a9cf4dad95f06e842c10d540287d737
vishnu303 <Shramavishal303@gmail.com>
2025-01-07 10:43:28 +0530

    Arraydequebasic

---
+++ Collectionjava/Arraydeque/Arraydequebasic.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+
+public class Arraydequebasic{
+    public static void main(String[] args) {
+        ArrayDeque<Integer>ad=new ArrayDeque<>();
+        ad.add(34);
+        ad.add(56);
+        ad.add(12);
+        System.out.println("Arraydeque"+ad);
+    }
+}
