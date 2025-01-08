b6a036be6330c29efab45c19741fc13b524325a7
vishnu303 <Shramavishal303@gmail.com>
2025-01-08 15:03:50 +0530

    ArraydequetoPriorityArray

--- Collectionjava/Arraydeque/ArraydequetoPriorityArray.java
+++ Collectionjava/Arraydeque/ArraydequetoPriorityArray.java
@@ -3,11 +3,15 @@ import java.util.PriorityQueue;
 
 public class ArraydequetoPriorityArray{
     public static void main(String[] args) {
-        
-        ArrayDeque<Integer>ad= new  PriorityQueue<>();
+  
+        ArrayDeque<Integer>ad= new  ArrayDeque<>();
         ad.add(23);
         ad.add(56);
         ad.add(45);
-        System.out.println("priorityQueue"+ad);
-    }
+        PriorityQueue<Integer>pq=new PriorityQueue<>(ad);
+        pq.add(23);
+        pq.add(12);
+        pq.add(34);
+        System.out.println(pq);
+        }
 }
