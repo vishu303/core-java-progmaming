7f6d12bb1ad7f6cba74fb6499800fa0c5ea93b0d
vishnu303 <Shramavishal303@gmail.com>
2025-01-08 16:18:02 +0530

    ArraydequeRemovemethod

---
+++ Collectionjava/Arraydeque/ArraydequeRemovemethod.java
@@ -0,0 +1,12 @@
+import java.util.ArrayDeque;
+
+public class ArraydequeRemovemethod {
+    public static void main(String[] args) {
+         ArrayDeque<Integer>ad= new  ArrayDeque<>();
+        ad.add(23);
+        ad.add(56);
+        ad.add(45);
+        ad.remove();
+        System.out.println(ad);
+    }
+}
