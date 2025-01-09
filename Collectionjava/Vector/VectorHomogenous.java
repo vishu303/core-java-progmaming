b495cc82daf67a463d03916c6301c0eb66cd536e
vishnu303 <Shramavishal303@gmail.com>
2025-01-09 15:22:53 +0530

    VectorHomogenous

---
+++ Collectionjava/Vector/VectorHomogenous.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+public class VectorHomogenous {
+    public static void main(String[] args) {
+        Vector<String>v=new Vector<>();
+        v.addElement("hii");;
+        v.add("red");
+        v.add("hello");
+        System.out.println(v);
+        
+    }
+}
