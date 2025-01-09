af8cd2d7c13bca5488d5e017132d49d3bcce6338
vishnu303 <Shramavishal303@gmail.com>
2025-01-09 15:16:26 +0530

    Vectorhetergenous

---
+++ Collectionjava/Vector/Vectorhetergenous.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+public class Vectorhetergenous{
+    public static void main(String[] args) {
+        Vector<Integer> v=new Vector<>();
+        v.addElement(23);
+        v.addElement(34);
+        v.addElement(14);
+        System.out.println(v);
+    }
+}
