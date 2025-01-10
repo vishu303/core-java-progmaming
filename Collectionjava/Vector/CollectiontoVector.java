5fbd311cff64f1b93e4694bb8fad8a04613da031
vishnu303 <Shramavishal303@gmail.com>
2025-01-10 20:47:28 +0530

    CollectiontoVector

---
+++ Collectionjava/Vector/CollectiontoVector.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CollectiontoVector {
+    public static void main(String[] args) {
+       Collection<Integer>c=new Vector<>();
+       c.add(23);
+       c.add(45);
+       c.add(90);
+       System.out.println(c);
+
+    }
+}
