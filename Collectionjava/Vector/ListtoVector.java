515b9ba3d5c985431f8bee7aa25213e2e0166ce7
vishnu303 <Shramavishal303@gmail.com>
2025-01-10 20:50:07 +0530

    ListtoVector

---
+++ Collectionjava/Vector/ListtoVector.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ListtoVector {
+    public static void main(String[] args) {
+        List<String>l=new Vector<>();
+         l.add("hii");
+         l.add("white");
+         l.add("may");
+         System.out.println(l);
+    }
+}
