5cf1bb7bcc01634b672defb5b66d4d1afdc21014
vishnu303 <Shramavishal303@gmail.com>
2025-01-15 20:29:37 +0530

    Wrapperclass

---
+++ Collectionjava/Stack/Wrapperclass.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Wrapperclass{
+    public static void main(String[] args) {
+        Stack<Integer>s=new Stack<>();
+        s.add(23);
+        s.add(56);
+        s.add(12);
+        System.out.println(s);
+    }
+}
