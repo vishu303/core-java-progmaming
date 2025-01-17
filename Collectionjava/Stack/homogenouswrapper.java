25e47aa3521f7881afb92081a5b78f95fcc24054
vishnu303 <Shramavishal303@gmail.com>
2025-01-17 20:24:22 +0530

    homogenouswrapper

---
+++ Collectionjava/Stack/homogenouswrapper.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class homogenouswrapper {
+    public static void main(String[] args) {
+        Stack<String>s=new Stack<>();
+           s.add("vishal");
+           s.add("hii");
+           s.add("red");
+           s.add("white");
+           System.out.println(s);
+    }
+}
