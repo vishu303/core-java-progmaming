59d60ac4cd5c3286abddc83466a9c9948ffbbafd
vishnu303 <Shramavishal303@gmail.com>
2025-02-06 10:35:57 +0530

    Returnstatment

---
+++ Recursivejava/Returnstatment.java
@@ -0,0 +1,12 @@
+class A{
+    static void show(){
+        System.out.println("upper statment");
+        return;
+    
+    }
+}
+public class Returnstatment {
+    public static void main(String[] args) {
+        A.show();
+    }
+}
