3100cf129576e48a3f76452cf1ef3d9adf5782cf
vishu303 <shramavishal303@gmail.com>
2025-03-01 21:17:03 +0530

    ThrowsExceptioncase2

---
+++ Exceptionhandling/ThrowsExceptioncase2.java
@@ -0,0 +1,8 @@
+public class ThrowsExceptioncase2  {
+    static void show()throws ArithmeticException{
+        System.out.println("Exception handle");
+    }
+    public static void main(String[] args) {
+        show();
+    }
+}
